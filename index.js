389470dc4b1b13c6051021f2985bf574d1cb4298
Hrithik Naha <naha.hrithkk@gmail.com>
2023-07-13 17:12:54 +0530

    add console messages

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5001;
 
 mongoose.connection.once("open", () => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
