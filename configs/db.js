d741211df65a988eabda7250ee23171066780af7
Hrithik Naha <naha.hrithkk@gmail.com>
2023-07-27 21:29:37 +0530

    add conditional db uri set based on env

--- configs/db.js
+++ configs/db.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
-const connectDB = () =>
+const connectDB = () => {
+    let URI = null;
+    if (process.env.NODE_ENV === "development") URI = process.env.NON_PROD_MONGO_URI;
+    else if (process.env.NODE_ENV === "production") URI = process.env.PROD_MONGO_URI;
+
     mongoose
-        .connect(process.env.NON_PROD_MONGO_URI, {
+        .connect(URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-        .then(() => console.log("Db Connected"))
+        .then(() => console.log("Db Connected :", process.env.NODE_ENV))
         .catch((err) => console.log(`${err} could not connect`));
+};
 
 module.exports = connectDB;
