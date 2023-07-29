b19d12499219b7a66700671ec75e3eaf147fbce2
Hrithik Naha <naha.hrithkk@gmail.com>
2023-07-29 21:07:57 +0530

    changed client domain

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "https://current-archive.netlify.app", "current-archive.netlify.app"];
 
 module.exports = allowedOrigins;
