48a0cb364160538e7d53203b2a8744cd518a1146
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-20 20:07:50 +0530

    samesite to none for cookie cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,7 +83,7 @@ const authController = {
                         res.cookie("jwt", refreshToken, {
                             httpOnly: true, //accessible only by web server
                             secure: true, //https
-                            sameSite: "None", //cross-site cookie
+                            sameSite: "none", //cross-site cookie
                             maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
                         });
 
