b0fc3f0d8f07e37eb9893906529a93af0c5b6286
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-24 14:49:24 +0530

    updated frontend api

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,8 +1,3 @@
-const allowedOrigins = [
-    "http://localhost:3000",
-    "http://192.168.1.11:3000",
-    "https://current-archive.netlify.app",
-    "current-archive.netlify.app",
-];
+const allowedOrigins = ["http://localhost:3000", "http://192.168.1.11:3000", "https://current-archive.onrender.com"];
 
 module.exports = allowedOrigins;
